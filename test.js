bd4a9aeca6b8f5fdf080763a77e78f39ecdccba1
Diego Nascimento <diego.nascimento@liferay.com>
2017-09-15 19:10:44 -0300

    correcting for travis integration

--- test.js
+++ test.js
@@ -5,48 +5,48 @@ const expect = require('chai').expect;
 describe('In person methods/attr', function(){
     //it verb is used to generate a case of test
     it('name should be a String', function(){
-        let p = new Person(20,20,'photos', 0);
-        let result = typeof (p.returnsName()) == 'string';
+        let obj = new Person(20,20,'photos', 0);
+        let result = typeof (obj.returnsName()) == 'string';
         //here, on expect we can define the expected value
         expect(result).to.equal(false);
     })
     it('age should be a Number', function(){
-        let p = new Person('Diego',false,'photos', 0);
-        let result = typeof p.returnsAge() == 'number';
+        let obj = new Person('Diego',false,'photos', 0);
+        let result = typeof obj.returnsAge() == 'number';
         expect(result).to.equal(false);
     })
     it('hobby should be a String', function(){
-        let p = new Person('Diego',20,false, 0);
-        let result = typeof p.returnsHobby() == 'string';
+        let obj = new Person('Diego',20,false, 0);
+        let result = typeof obj.returnsHobby() == 'string';
         expect(result).to.equal(false);
     })
     it('addBudget cannot return other string another "positive value please <3"', function(){
-      let p = new Person('Diego',20,'photo', 0);
-      let result = p.addBudget(-1);
+      let obj = new Person('Diego',20,'photo', 0);
+      let result = obj.addBudget(-1);
       expect(result).to.equal("positive value please <3");
     })
     it('debitBudget cannot return other string another "positive value please <3"', function(){
-      let p = new Person('Diego',20,'photo', 0);
-      let result = p.debitBudget(-1);
+      let obj = new Person('Diego',20,'photo', 0);
+      let result = obj.debitBudget(-1);
       expect(result).to.equal(false);
     })
     it('changeHobby must return a string', function(){
-      let p = new Person('Diego',20,'photo', 0);
-      let result = typeof p.changeHobby() == 'string';
+      let obj = new Person('Diego',20,'photo', 0);
+      let result = typeof obj.changeHobby() == 'string';
       expect(result).to.equal(false);
     })
     it('returnsBudget must return a number', function(){
-      let p = new Person('Diego',20,'photo', 0);
-      let result = typeof p.returnsBudget() == 'number';
+      let obj = new Person('Diego',20,'photo', 0);
+      let result = typeof obj.returnsBudget() == 'number';
       expect(result).to.equal(true);
     })
     it('debitBudget cannot receive a negative number', function(){
-      let p = new Person('Diego',20,'photo', 10);
-      let result = p.debitBudget(-10);
+      let obj = new Person('Diego',20,'photo', 10);
+      let result = obj.debitBudget(-10);
       expect(result).to.equal(false);
 
-      let p2 = new Person('Diego',20,'photo', 100);
-      let result2 = p2.debitBudget(2);
+      let obj2 = new Person('Diego',20,'photo', 100);
+      let result2 = obj2.debitBudget(2);
       expect(result2).to.equal(true);
     })
     it('addBudget should receive a posive number', function(){
